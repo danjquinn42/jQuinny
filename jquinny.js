3384b475f7fd95b59289d7bbd30a3249909b5e41
danjquinn42 <danjquinn42@gmail.com>
2016-12-20 17:27:02 -0500

    Revert to ES5 notation for compatibility

--- jquinny.js
+++ jquinny.js
@@ -42,9 +42,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	import DOMNodeCollection from './dom_node_collection.js';
+	const DOMNodeCollection = __webpack_require__(1);
 	
 	let functionQueue = [];
 	
@@ -144,7 +144,136 @@
 	
 	
 	window.jQuinny = jQuinny;
-	export default jQuinny;
+	module.exports = jQuinny;
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	
+	class DOMNodeCollection {
+	  constructor(nodes) {
+	    this.nodes = nodes;
+	  }
+	
+	  first() {
+	    return this.nodes[0];
+	  }
+	
+	  last() {
+	    return this.nodes[this.nodes.length - 1];
+	  }
+	
+	  each(callback) {
+	    this.node.forEach(callback);
+	  }
+	
+	  map(callback) {
+	    this.node.map(callback);
+	  }
+	
+	  html(html) {
+	    if (typeof html === 'string') {
+	      this.each(node => {
+	        node.innerHTML = html;
+	      });
+	    } else {
+	      return this.nodes.first().innerHTML;
+	    }
+	  }
+	
+	  empty() {
+	    this.html('');
+	  }
+	
+	  isEmpty() {
+	    return (this.nodes.length === 0);
+	  }
+	
+	
+	  append(collection) {
+	    if (this.isEmpty()) return;
+	
+	    if (typeof collection === 'object' &&
+	      !(collection instanceof DOMNodeCollection)) {
+	        collection = window.jQuinny(collection);
+	    }
+	
+	    if (typeof collection === 'string') {
+	      this.each( node => { node.innerHTML += collection;});
+	    } else if (collection instanceof DOMNodeCollection) {
+	      this.each(node => {
+	        collection.each(collectionNode => {
+	          node.appendChild(collectionNode.cloneNode(true));
+	        });
+	      });
+	    }
+	  }
+	
+	  attr(attributeName, value) {
+	    if (arguments.length === 1) {
+	      return this.first().getAttribute(attributeName);
+	    } else if (arguments.length === 2) {
+	      this.each( node => node.setAttribute(attributeName, value));
+	    }
+	  }
+	
+	  children() {
+	    let children = [];
+	    this.each(node => {
+	      children = children.concat(Array.from(node.children));
+	    });
+	    return new DOMNodeCollection(children);
+	  }
+	
+	  parent() {
+	    const parents = [];
+	    this.each(node => parents.push(node.parentNode));
+	    return new DOMNodeCollection(parents);
+	  }
+	
+	  find(selector) {
+	    let found = [];
+	    this.each(node => {
+	      const nodeList = node.querySelectorAll(selector);
+	      found = found.concat(Array.from(nodeList));
+	    });
+	    return new DOMNodeCollection(found);
+	  }
+	
+	  remove(){
+	    this.each(node => node.remove());
+	  }
+	
+	  addClass(cssClass) {
+	    this.each(node => node.classList.add(cssClass));
+	  }
+	
+	  removeClass(cssClass) {
+	    this.each(node => node.classList.remove(cssClass));
+	  }
+	
+	  on(event, callback) {
+	    this.each(node =>{
+	      node.addEventListener(event, callback);
+	      node[event].push(callback);
+	    });
+	  }
+	
+	  off(eventName) {
+	    this.each(node =>{
+	      if (node[event]) {
+	        node[event].forEach((callback) => {
+	          node.removeEventListener(eventName, callback);
+	        });
+	      }
+	      node[event] = [];
+	    });
+	  }
+	}
+	
+	module.exports = DOMNodeCollection;
 
 
 /***/ }
